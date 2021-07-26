b8665f8e0ec912827c5d60c8b9db5d2ec3be45a2
EmTori <emily.houlihan@outlook.com>
2021-07-25 21:34:12 -0230

    added trivia questions to javascript file

---
+++ trivia.js
@@ -0,0 +1,252 @@
+let myQuestions = [
+    {
+      question: "Where Is The Avengers Tower Located?",
+      answers: {
+        a: "Brooklyn",
+        b: "Queens",
+        c: "New York City",
+        d: "Manhattan"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "According To Tony Stark, What Is Phil Coulson's First Name?",
+      answers: {
+        a: "Sergeant",
+        b: "Agent",
+        c: "Officer",
+        d: "Director"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "How Many Years Was Captain American Frozen In Ice?",
+      answers: {
+        a: "70 Years",
+        b: "60 Years",
+        c: "100 Years",
+        d: "90 Years"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What Is Hawkeye's Real Name?",
+      answers: {
+        a: "Clint Archer",
+        b: "Oliver Queen",
+        c: "Clint Barton",
+        d: "Ronin"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Who Pulls Captain America's Unconscious Body Out Of The Potomac River?",
+      answers: {
+        a: "Sam Wilson",
+        b: "Tony Stark",
+        c: "S.H.I.E.L.D",
+        d: "The Winter Soldier"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What Is Howard Stark's “Greatest Creation?",
+      answers: {
+        a: "Captain America's Shield",
+        b: "His Child",
+        c: "Arc Reactor",
+        d: "Stark Industries"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What Year Was The First Iron Man Movie Released?",
+      answers: {
+        a: "2009",
+        b: "2007",
+        c: "2006",
+        d: "2008"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What Is Captain America’s Shield Made Out Of?",
+      answers: {
+        a: "Vibranium",
+        b: "Titanium",
+        c: "Adamantium",
+        d: "Carbonadium"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What Country Are Scarlet Witch And Quicksilver From?",
+      answers: {
+        a: "Russia",
+        b: "Sokovia",
+        c: "Germany",
+        d: "Slovakia"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Who Was The Last Holder Of The Space Stone Before Thanos?",
+      answers: {
+        a: "Hydra",
+        b: "The Collector",
+        c: "Loki",
+        d: "The Sorcerer Supreme"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What Is NOT A Colour Of An Infinity Stone?",
+      answers: {
+        a: "Black",
+        b: "Orange",
+        c: "Blue",
+        d: "Green"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What Is The Name Of The Universe Ant-Man Travels To When He Goes Subatomic?",
+      answers: {
+        a: "The Astral Realm",
+        b: "The Subatomic Realm",
+        c: "The Atomic Realm",
+        d: "The Quantum Realm"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What Is The Name Of The Treaty Which Divides The Avengers Into Opposing Factions?",
+      answers: {
+        a: "The Hero Accords",
+        b: "The Sokovia Accords",
+        c: "The S.H.I.E.L.D Accords",
+        d: "The Wakanda Accords"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which Of The Infinity Stones Is Hidden On Vormir?",
+      answers: {
+        a: "The Soul Stone",
+        b: "The  Mind Stone",
+        c: "The Reality Stone",
+        d: "The Power Stone"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What Does Dr.Strange Use To Control Time?",
+      answers: {
+        a: "The Time Chair",
+        b: "The Tesseract",
+        c: "The Eye Of Agamotto",
+        d: "The Heart of the Universe"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "How Tall Was Steve Rogers Before Taking The Super Soldier Serum?",
+      answers: {
+        a: "5’7”",
+        b: "5’9”",
+        c: "5’6”",
+        d: "5’4”"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "In “Thor: The Dark World”, Who Does Loki Turn Himself Into?",
+      answers: {
+        a: "Black Widow",
+        b: "Captain America",
+        c: "Hawkeye",
+        d: "Iron Man"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Who Destroys Thor's Hammer?",
+      answers: {
+        a: "Hela",
+        b: "Loki",
+        c: "Thanos",
+        d: "Ultron"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What Memento Does Peter Quill Have From His Mother?",
+      answers: {
+        a: "A Teddy Bear",
+        b: "A Photograph",
+        c: "A Letter",
+        d: "A Mix Tape"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What's The Name Of The AI Tony Stark Invented To Replace J.A.R.V.I.S.?",
+      answers: {
+        a: "E.D.I.T.H",
+        b: "F.R.I.D.A.Y",
+        c: "S.H.I.E.L.D",
+        d: "U.L.T.R.O.N"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "In Which Film Did Black Widow First Appear?",
+      answers: {
+        a: "Iron Man 2",
+        b: "Thor",
+        c: "Iron Man",
+        d: "Captain America The First Avenger"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What Is The Name Of The Alien Race Loki Teams Up With In The Avengers?",
+      answers: {
+        a: "The Frost Giants",
+        b: "The Kree",
+        c: "The Skrull",
+        d: "The Chitauri"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Who Can Lift Thor’s Hammer?",
+      answers: {
+        a: "Loki",
+        b: "Bucky Barnes",
+        c: "Captain America",
+        d: "Iron Man"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What Color Does Agatha’s Magic Appear In WandaVision?",
+      answers: {
+        a: "Purple",
+        b: "Black",
+        c: "Red",
+        d: "Blue"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Which Of These Does Tony Stark NOT Describe Himself As?",
+      answers: {
+        a: "Philanthropist",
+        b: "Playboy",
+        c: "Millionaire",
+        d: "Scientist "
+      },
+      correctAnswer: "d"
+    }
+  ];
